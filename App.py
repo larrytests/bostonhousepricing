4180784ddb58680c4fdc0461e367a735e8f0e4c5
Aryan Verma <larrylab2001@tutanota.com>
2024-07-12 12:30:58 +0530

    Final Web App

---
+++ App.py
@@ -0,0 +1,38 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model
+model = pickle.load(open('regmodel.pkl', 'rb'))
+scalar1 = pickle.load(open('scalar.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar1.transform(np.array(list(data.values())).reshape(1, -1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar1.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template('home.html', prediction_text="The House price prediction is {}".format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
